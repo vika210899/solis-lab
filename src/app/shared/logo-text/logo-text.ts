f0d3e9501c878daf718602f0d7d21ff332ed5548
Viktoria Tereshchenko <vika2108991@gmail.com>
2026-06-02 15:43:34 +0300

    logo text shared element

---
+++ src/app/shared/logo-text/logo-text.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-text',
+  imports: [],
+  templateUrl: './logo-text.html',
+  styleUrl: './logo-text.scss',
+})
+export class LogoText {
+
+}
