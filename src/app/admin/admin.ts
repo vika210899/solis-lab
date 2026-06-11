6dc3b68371f6e1955377f2d7e54000b43752a8f2
Viktoria Tereshchenko <vika2108991@gmail.com>
2026-06-11 20:57:31 +0300

    added simple button shared element & red color for styling

--- src/app/admin/admin.ts
+++ src/app/admin/admin.ts
@@ -4,10 +4,11 @@ import { CategoriesService } from '../categories/categories.service';
 import { NewItem } from './new-item/new-item';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MenuSide } from '../shared/menu-side/menu-side';
+import { Button } from '../shared/button/button';
 
 @Component({
   selector: 'app-admin',
-  imports: [ProductsAdmin, NewItem, MatProgressSpinnerModule, MenuSide],
+  imports: [ProductsAdmin, NewItem, MatProgressSpinnerModule, MenuSide, Button],
   templateUrl: './admin.html',
   styleUrl: './admin.scss',
 })
---
+++ src/app/shared/button/button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[appButton]',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+})
+export class Button {}
