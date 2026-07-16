747f17e9344d1966dfc1ab2e5129a57374e27ed6
Viktoria Tereshchenko <vika2108991@gmail.com>
2026-07-16 21:22:36 +0300

    added: header routing

--- src/app/app-header/app-header.ts
+++ src/app/app-header/app-header.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, RouterLink, FormsModule],
   templateUrl: './app-header.html',
   styleUrl: './app-header.scss',
 })
-export class AppHeader {}
+export class AppHeader {
+  isOpen: boolean = false;
+  newName = '';
+
+  onStartAuth(event: Event) {
+    console.log('start');
+    event.stopPropagation();
+    this.isOpen = true;
+  }
+
+  onAuth() {
+    console.log('AUTH!', this.newName);
+    this.newName = '';
+    this.isOpen = false;
+  }
+
+  onCancel() {
+    this.isOpen = false;
+  }
+}
