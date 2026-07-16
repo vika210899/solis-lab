0cac31960b319c3aeb75ee25c25552a183822168
Viktoria Tereshchenko <vika2108991@gmail.com>
2026-07-16 20:11:49 +0300

    added: delivery component plug

---
+++ src/app/app-footer/delivery-component/delivery-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-component',
+  imports: [],
+  templateUrl: './delivery-component.html',
+  styleUrl: './delivery-component.scss',
+})
+export class DeliveryComponent {
+
+}
